d8faf15ae617132b7dc78af92dc86342fd890414
Oscar Urizar <oscar.m.Urizar.jr@gmail.com>
2021-11-24 09:58:24 -0500

    update config file

--- config/config.js
+++ config/config.js
@@ -2,6 +2,7 @@ const Sequelize = require('sequelize');
 require('dotenv').config();
 
 const sequelize = new Sequelize(
+    process.env.JAWSDB_URL ||
     process.env.DB_NAME,
     process.env.DB_USER,
     process.env.DB_PW, {
